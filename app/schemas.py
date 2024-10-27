379281f013da99c5bd5534f6a8e1e5dd4fd8bd4a
Yusuf <yusuf.acmaci01@gmail.com>
2024-10-27 23:45:11 +0300

    Planlanan route'lar için şemalar hazırlandı

--- app/schemas.py
+++ app/schemas.py
@@ -2,9 +2,25 @@ from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
     email: EmailStr
+    first_name: str
+    last_name: str
     password: str
     is_teacher: bool = False
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class ClassBase(BaseModel):
+    class_name: str
+    university_name: str
+
+class JoinClassRequest(BaseModel):
+    invite_code: str
+
+class TaskUpdate(BaseModel):
+    task_description: str
+    task_language: str
+
+class CodeExecutionRequest(BaseModel):
+    code: str
